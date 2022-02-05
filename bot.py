624f123e8c19364ce1f8ac202dc37ddb338e841d
KuborKelp <78284261+KuborKelp@users.noreply.github.com>
2022-02-06 00:00:29 +0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 from graia.ariadne.app import Ariadne
 from graia.ariadne.model import MiraiSession
 from graia.saya import Saya
@@ -13,9 +10,9 @@ if __name__ == '__main__':
     app = Ariadne(
         MiraiSession(
             host="http://localhost:8080",  # 填入 HTTP API 服务运行的地址
-            verify_key="sbsnz666",  # 填入 verifyKey
+            verify_key="1145abgg",  # 填入 verifyKey
             account=2424947232,  # 你的机器人的 qq 号
-        ),
+        )
     )
 
     saya = Saya(app.broadcast)
@@ -27,15 +24,10 @@ if __name__ == '__main__':
 
     with saya.module_context():
         path = './modules/'
-        saya.require("modules.test")
         files = os.listdir(path)
         for i in files:
             if i[-3:] == '.py' and 'test' not in i:
                 saya.require(f"modules.{i[:-3]}")    
-        '''
-        saya.require("modules.test")
-        '''
-
     try:
         app.loop.run_until_complete(main())
     except KeyboardInterrupt:
